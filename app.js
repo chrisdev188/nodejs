7ec55bc6d44b189bb0f64f24aaf779565f9d26da
Chris Bui <chrisbui.dev188@gmail.com>
2023-01-14 04:29:15 +0700

    Use logging and static files middleware

--- app.js
+++ app.js
@@ -1,26 +1,43 @@
 const express = require("express");
 
+// logger middleware
+const morgan = require("morgan");
+
 // create an express app
 const app = express();
 const port = 3000;
-
-// register view engine
-app.set("view engine", "ejs");
 
 app.listen(port, () => {
   console.log("Listening for requests on port " + port);
 });
 
+// register view engine
+app.set("view engine", "ejs");
+
+// use static files
+app.use(express.static("public"));
+
+// use logger middleware
+app.use(morgan("dev"));
+
 app.get("/", (req, res) => {
-  res.render("index", { title: "Home" });
+  const blogs = [
+    {
+      title: "How to set up React project with Vite and Tailwindcss.",
+      description:
+        "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.",
+    },
+    {
+      title: "How to learn Nodejs.",
+      description:
+        "Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
+    },
+  ];
+  res.render("index", { title: "Blogs", blogs });
 });
 
 app.get("/about", (req, res) => {
   res.render("about", { title: "About" });
-});
-
-app.get("/blogs", (req, res) => {
-  res.render("blogs", { title: "Blogs" });
 });
 
 app.get("/blogs/create", (req, res) => {
@@ -29,5 +46,5 @@ app.get("/blogs/create", (req, res) => {
 
 // when none of those above matched, send 404 html page.
 app.use((req, res) => {
-  res.status(404).render("404");
+  res.status(404).render("404", { title: "404" });
 });
